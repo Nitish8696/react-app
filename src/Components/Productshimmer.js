e6948abb57f6bf22205a1f09e08077fe08c00e85
Nitish <nitihchoudhary8696@gmail.com>
2023-07-27 13:57:35 +0530

    shimmer effect update

--- src/Components/Productshimmer.js
+++ src/Components/Productshimmer.js
@@ -4,26 +4,26 @@ import Skeleton from '@mui/material/Skeleton';
 const Productshimmer = () => {
     return (
         <div className='max-w-screen-2xl mx-auto grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-5 px-4 bg-gray-50 py-4 mt-[-40px] sm:mt-[-100px]'>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
-            <Skeleton variant="rectangular" width={210} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
+            <Skeleton variant="rectangular" width={190} height={300} className="z-30 "/>
         </div>
     )
 }
